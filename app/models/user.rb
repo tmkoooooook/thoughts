27dd438e3053599f57cc2a910bbbc9cf4fcbb91a
BuildTools <unconfigured@null.spigotmc.org>
2021-06-16 13:57:45 +0900

    devise導入

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true, uniqueness: true
+end
